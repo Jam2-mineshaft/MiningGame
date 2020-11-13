66d125c7b0237fcac70e29fc68bee96ecef05200
Joseph <josephgarrad2017@gmail.com>
2020-11-13 18:05:19 +0000

    added movement, turening and sounds
    working on movement

---
+++ MiningJam/Assets/Players/Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+    AudioSource audsource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        audsource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(cc.velocity.magnitude > 1f && audsource.isPlaying == false)
+        {
+            audsource.volume = Random.Range(0.7f, 1);
+            audsource.pitch = Random.Range(0.8f, 1.2f);
+            audsource.Play();
+        }
+    }
+}
---
+++ MiningJam/Assets/Players/Player2movement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2movement : MonoBehaviour
+{
+    private CharacterController controller;
+    AudioSource asource;
+    public GameObject Player1;
+    public GameObject Player2;
+    public int Speed;
+    public int whatplayeristhis;
+
+    void Start()
+    {
+        asource = GetComponent<AudioSource>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(whatplayeristhis == 0)
+        {
+            MovementP1();
+        }
+        if(whatplayeristhis ==1)
+        {
+            MovementP2();
+        }
+      
+       
+    }
+    void MovementP2()
+    {
+        Vector2 moveinput = new Vector2(Input.GetAxis("P2-leftjoyhori"), Input.GetAxis("P2-leftjoyverti"));
+        Vector2 LookDir = new Vector2(Input.GetAxis("P2-rightjoyhori"), Input.GetAxis("P2-rightjoyverti"));
+        
+
+
+
+        Vector3 forward = transform.forward; //transform.forward;
+        Vector3 right = transform.right; //transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 DesMoveDir = (forward * -moveinput.y + right * moveinput.x).normalized;
+        Vector3 DesLookDir = (Vector3.right * LookDir.x + Vector3.forward * -LookDir.y).normalized;
+
+
+        controller.Move(DesMoveDir * Speed * Time.deltaTime);
+        if (DesLookDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(DesLookDir.normalized), 0.15f);
+        }
+    }
+
+
+    void MovementP1()
+    {
+        Vector2 moveinput = new Vector2(Input.GetAxis("P1-leftjoyhori"), Input.GetAxis("P1-leftjoyverti"));
+        Vector2 LookDir = new Vector2(Input.GetAxis("P1-rightjoyhori"), Input.GetAxis("P1-rightjoyverti"));
+
+
+
+
+        Vector3 forward = transform.forward; //transform.forward;
+        Vector3 right = transform.right; //transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 DesMoveDir = (forward * -moveinput.y + right * moveinput.x).normalized;
+        Vector3 DesLookDir = (Vector3.forward * -LookDir.y +Vector3.right * LookDir.x).normalized;
+   
+
+        controller.Move(DesMoveDir * Speed * Time.deltaTime);
+        if (DesLookDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(DesLookDir.normalized), 0.15f);
+        }
+    }
+}
