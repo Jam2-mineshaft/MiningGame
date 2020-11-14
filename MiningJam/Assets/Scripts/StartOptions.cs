e40940d4ec3355d846f53a1bc1a252d195302811
Samsei <samuel3.carr@live.uwe.ac.uk>
2020-11-14 13:13:37 +0000

    i made a menu

---
+++ MiningJam/Assets/Scripts/StartOptions.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+
+public class StartOptions : MonoBehaviour {
+
+
+
+	public int sceneToStart = 1;									
+	public bool changeScenes;											
+	public bool changeMusicOnStart;									
+	public int musicToChangeTo = 0;									
+
+
+	[HideInInspector] public bool inMainMenu = true;				
+	
+
+
+	private PlayMusic playMusic;										
+	private float fastFadeIn = .01f;								
+	private ShowPanels showPanels;										
+
+	
+	void Awake()
+	{
+		showPanels = GetComponent<ShowPanels> ();
+
+		playMusic = GetComponent<PlayMusic> ();
+	}
+
+
+	public void StartButtonClicked()
+	{
+		if (changeScenes) 
+		{
+			//CHANGE SCENE FROM START MENU
+		} 
+
+		else 
+		{
+			StartGameInScene();
+		}
+
+	}
+
+
+	public void LoadDelayed()
+	{
+		inMainMenu = false;
+
+		showPanels.HideMenu ();
+
+		Application.LoadLevel (sceneToStart);
+	}
+
+
+	public void StartGameInScene()
+	{
+		inMainMenu = false;
+
+		//CHANGE SCENE WHEN START PRESS
+	}
+
+
+	public void PlayNewMusic()
+	{
+		//CHANGE MUSIC TO BE ADDED
+	}
+
+	public void HideDelayed()
+	{
+		showPanels.HideMenu();
+	}
+}
