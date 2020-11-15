94c4ffc31d88a8a92b627032a8dcce19bf77f07c
Gamble <joe2.gamble@live.uwe.ac.uk>
2020-11-15 18:15:40 +0000

    Cave In particles and distance based camera shake

--- MiningJam/Assets/Scripts/TrackSpawner.cs
+++ MiningJam/Assets/Scripts/TrackSpawner.cs
@@ -12,10 +12,28 @@ public class TrackSpawner : MonoBehaviour
 
     float next_spawn_position = 135f;
 
+    //private CaveManager caveManager;
+    //public List<GameObject> sections;
+
+    //public List<GameObject> prefabPool = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+        int start_loc = -135;
+
+        sections = new List<GameObject>();
+
+        caveManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<CaveManager>();
+
+        for (int i = 0; i < prefabPool.Count; i++)
+        {
+            sections.Add(GameObject.Instantiate(prefabPool[i]));
+            sections[i].transform.Translate(new Vector3(0, 0, start_loc));
+            start_loc += 135;
+        }
+        */
     }
 
     // Update is called once per frame
@@ -25,6 +43,14 @@ public class TrackSpawner : MonoBehaviour
         {
             Instantiate(generic_section_prefabs[Random.Range(0, generic_section_prefabs.Length)], new Vector3(0, 0, next_spawn_position), new Quaternion(0, 0, 0, 0));
             next_spawn_position += section_length;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Section_Detection"))
+        {
+
         }
     }
 }
