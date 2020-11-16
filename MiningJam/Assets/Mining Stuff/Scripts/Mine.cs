42dc83855b4ee38a3df1a2120320bf0799bc5cda
HJNewton <43638999+HJNewton@users.noreply.github.com>
2020-11-16 13:57:21 +0000

    Added mining, cleaned up terrain prefabs
    Couldn't hit many of the rocks so moved them about

--- MiningJam/Assets/Mining Stuff/Scripts/Mine.cs
+++ MiningJam/Assets/Mining Stuff/Scripts/Mine.cs
@@ -7,17 +7,17 @@ using UnityEngine.Audio;
 public class Mine : MonoBehaviour
 {
     [Header("Setup")]
+    public int whatPlayerIsThis;
     public float mineReach;
     public MinableObjectsSO coalSettings;
     public MinableObjectsSO goldSettings;
     public int currentCoal;
     public int currentGold;
-    public Text coalText;
-    public Text goldText;
+    //public Text coalText;
+    //public Text goldText;
 
     [Header("Audio Setup")]
-    public AudioClip[] coalMiningSounds;
-    public AudioClip[] goldMiningSounds;
+    public GameObject audioManager;
 
     RaycastHit hit;
 
@@ -28,44 +28,64 @@ public class Mine : MonoBehaviour
 
     void MineObject()
     {
-        // Coal Functionality
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, mineReach) && hit.transform.tag == "Coal") // Casts a ray from the player and checks tag
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * mineReach);
+
+        if (whatPlayerIsThis == 0)
         {
-            if (Input.GetKeyDown(KeyCode.Space)) // If player presses correct input then add value to coal, change UI text and destroy the coal block
+            // Coal Functionality
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mineReach) && hit.transform.tag == "Coal") // Casts a ray from the player and checks tag
             {
-                currentCoal += coalSettings.valueToGive;
-                coalText.text = currentCoal.ToString();
-                Instantiate(coalSettings.destructionParticles, hit.transform.position, Quaternion.identity);
-                PlayCoalAudio(); // Plays mining audio
-                Destroy(hit.transform.gameObject);
+                if (Input.GetKeyDown(KeyCode.Joystick1Button0)) // If player presses correct input then add value to coal, change UI text and destroy the coal block
+                {
+                    currentCoal += coalSettings.valueToGive;
+                    //coalText.text = currentCoal.ToString();
+                    Instantiate(coalSettings.destructionParticles, hit.transform.position, Quaternion.identity);
+                    audioManager.GetComponent<PlayAudio>().PlayCoalAudio(); // Plays mining audio
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+
+            // Gold Functionality
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mineReach) && hit.transform.tag == "Gold") // Casts a ray from the player and checks tag
+            {
+                if (Input.GetKeyDown(KeyCode.Joystick1Button0)) // If player presses correct input then add value to coal, change UI text and destroy the coal block -- CHANGE SO IT FUNCTIONS WITH EACH PLAYER'S JOYSTICK
+                {
+                    currentGold += goldSettings.valueToGive;
+                    //goldText.text = currentGold.ToString();
+                    Instantiate(goldSettings.destructionParticles, hit.transform.position, Quaternion.identity);
+                    audioManager.GetComponent<PlayAudio>().PlayGoldAudio(); // Plays mining audio
+                    Destroy(hit.transform.gameObject);
+                }
             }
         }
 
-        // Gold Functionality
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, mineReach) && hit.transform.tag == "Gold") // Casts a ray from the player and checks tag
+        if (whatPlayerIsThis == 1)
         {
-            if (Input.GetKeyDown(KeyCode.Space)) // If player presses correct input then add value to coal, change UI text and destroy the coal block -- CHANGE SO IT FUNCTIONS WITH EACH PLAYER'S JOYSTICK
+            // Coal Functionality
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mineReach) && hit.transform.tag == "Coal") // Casts a ray from the player and checks tag
             {
-                currentGold += goldSettings.valueToGive;
-                goldText.text = currentGold.ToString();
-                Instantiate(goldSettings.destructionParticles, hit.transform.position, Quaternion.identity);
-                PlayGoldAudio(); // Plays mining audio
-                Destroy(hit.transform.gameObject);
+                if (Input.GetKeyDown(KeyCode.Joystick2Button0)) // If player presses correct input then add value to coal, change UI text and destroy the coal block
+                {
+                    currentCoal += coalSettings.valueToGive;
+                    //coalText.text = currentCoal.ToString();
+                    Instantiate(coalSettings.destructionParticles, hit.transform.position, Quaternion.identity);
+                    audioManager.GetComponent<PlayAudio>().PlayCoalAudio(); // Plays mining audio
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+
+            // Gold Functionality
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, mineReach) && hit.transform.tag == "Gold") // Casts a ray from the player and checks tag
+            {
+                if (Input.GetKeyDown(KeyCode.Joystick2Button0)) // If player presses correct input then add value to coal, change UI text and destroy the coal block -- CHANGE SO IT FUNCTIONS WITH EACH PLAYER'S JOYSTICK
+                {
+                    currentGold += goldSettings.valueToGive;
+                    //goldText.text = currentGold.ToString();
+                    Instantiate(goldSettings.destructionParticles, hit.transform.position, Quaternion.identity);
+                    audioManager.GetComponent<PlayAudio>().PlayGoldAudio(); // Plays mining audio
+                    Destroy(hit.transform.gameObject);
+                }
             }
         }
-    }
-
-    void PlayCoalAudio()
-    {
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.clip = coalMiningSounds[Random.Range(0, coalMiningSounds.Length)];
-        audio.Play();
-    }
-
-    void PlayGoldAudio()
-    {
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.clip = goldMiningSounds[Random.Range(0, goldMiningSounds.Length)];
-        audio.Play();
     }
 }
