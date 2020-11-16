d86c505c75431029fa14ccbab26e7312ce7415ff
Samsei <samuel3.carr@live.uwe.ac.uk>
2020-11-16 20:12:27 +0000

    adding stuff

---
+++ MiningJam/Assets/Scripts/DrawHighscores.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawHighscores : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    private int[] scores;
+
+
+    void Start()
+    {
+        string json = file.ReadToEnd();
+        List<Item> score = JsonConvert.DeserializeObject<List<Item>>(json);
+
+        /*for (int i = 0; i < 5; i++)
+        {
+            scores[i] = JsonUtility.FromJson<scores>(jsonFile.text);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
